6a1dc19919f73fac9f53003b531ebb14061ab933
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-17 16:37:04 -0500

    Add styling for Sign Up Now element

--- components/OfferBar.tsx
+++ components/OfferBar.tsx
@@ -3,7 +3,8 @@
 const OfferBar = () => {
     return ( 
         <div className="h-8 cursor-default w-full bg-black text-white md:text-sm text-xs font-satoshi flex items-center justify-center">
-            <p className="px-3">Sign up and get 20% off to your first order.</p><a href='/' className="underline">Sign Up Now</a>
+            <p className="px-3">Sign up and get 20% off to your first order.</p>
+            <a onClick={()=>{}} className="underline cursor-pointer">Sign Up Now</a>
         </div> 
     );
 }
