8389f4553ec7d2129f4fbcca16c77059c19e4aa2
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-17 16:37:33 -0500

    Update OfferBar.tsx

--- components/OfferBar.tsx
+++ components/OfferBar.tsx
@@ -4,7 +4,7 @@ const OfferBar = () => {
     return ( 
         <div className="h-8 cursor-default w-full bg-black text-white md:text-sm text-xs font-satoshi flex items-center justify-center">
             <p className="px-3">Sign up and get 20% off to your first order.</p>
-            <a onClick={()=>{}} className="underline cursor-pointer">Sign Up Now</a>
+            <a onClick={() => {}} className="underline cursor-pointer">Sign Up Now</a>
         </div> 
     );
 }
