c303ae71eaa16e6336350e09bc870433f2cadf8e
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-17 15:57:52 -0500

    Upload local fonts and add OfferBar code

---
+++ components/OfferBar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const OfferBar = () => {
+    return ( 
+        <div className="h-8 w-full bg-black text-white md:text-sm text-xs font-satoshi flex items-center justify-center">
+            <p className="px-3">Sign up and get 20% off to your first order.</p><a href='/' className="underline">Sign Up Now</a>
+        </div> 
+    );
+}
+ 
+export default OfferBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'satoshi': ["satoshi"]
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
