2edf50c1b279a5f1734d9aa8f0ee5a80d759fcc0
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-17 16:05:49 -0500

    Change cursor

--- components/OfferBar.tsx
+++ components/OfferBar.tsx
@@ -2,7 +2,7 @@
 
 const OfferBar = () => {
     return ( 
-        <div className="h-8 w-full bg-black text-white md:text-sm text-xs font-satoshi flex items-center justify-center">
+        <div className="h-8 cursor-default w-full bg-black text-white md:text-sm text-xs font-satoshi flex items-center justify-center">
             <p className="px-3">Sign up and get 20% off to your first order.</p><a href='/' className="underline">Sign Up Now</a>
         </div> 
     );
