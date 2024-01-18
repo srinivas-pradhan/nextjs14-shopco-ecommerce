7dae9969c8b55f2f770c9304ef4dd5439b47ec6d
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-01-17 23:08:51 -0500

    Update OfferBar.tsx

--- components/OfferBar.tsx
+++ components/OfferBar.tsx
@@ -3,26 +3,24 @@ import Image from "next/image";
 
 const OfferBar = () => {
     return ( 
-        <div className="bg-black h-8 w-full flex justify-between items-center">
+        <div className="bg-black h-10 w-full flex justify-between items-center">
             <div className="w-[85%] flex items-center justify-center mx-auto">
                 <ul className="flex items-center justify-center mx-auto">
-                    <li className="text-white font-satoshi md:text-sm text-xs px-3">
+                    <li className="text-white font-satoshi md:text-base text-xs px-3">
                         Sign up and get 20% off to your first order.
                     </li>
-                    <li className="underline cursor-pointer text-white font-satoshi md:text-sm text-xs"
+                    <li className="underline cursor-pointer text-white font-satoshi md:text-base text-xs"
                         onClick={() =>{}}
                     >
                     Sign Up Now
                     </li>
                 </ul>
                 <div className="cursor-pointer text-white font-satoshi md:text-sm text-xs hidden md:block"
-                    onClick={() => {
-                        console.log("Clicked")
-                    }}
+                    onClick={() => {}}
                 >
                     <Image
-                        height={10}
-                        width={10}
+                        height={15}
+                        width={15}
                         src="/elements/X.png"
                         alt=""
                     />
